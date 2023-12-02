c1ccc546c6c63ae9e4a3736e175872852d292bf0
Barraguesh <25356150+Barraguesh@users.noreply.github.com>
2023-12-02 23:20:36 +0100

    Modularizacion de clases y revert de rama mala

--- MensajeEncriptado/src/cifrado/Cifrado.java
+++ MensajeEncriptado/src/cifrado/Cifrado.java
@@ -1,7 +1,24 @@
 package cifrado;
 
-import javax.crypto.KeyGenerator;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 
 public class Cifrado {
 	
+	//Objeto que nos permitira encriptar
+	static Cipher cifrador;
+	
+	public static Cipher getCifrador() {
+		return cifrador;
+	}
+
+	public Cifrado(SecretKey claveSecreta) {
+		try {
+			cifrador = Cipher.getInstance("DES");
+			//Ahora el cifrador lo configuramos para que use la clave simetrica para encriptar
+			cifrador.init(Cipher.ENCRYPT_MODE, claveSecreta);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
 }
